71118d57a943d21ceb17cc38f968242527a4dac8
jessicagomesdossantos <jessicagomesdossantos@192.168.0.106>
2015-03-31 14:04:31 -0300

    Commit de criação das classes do projeto

--- IMC/src/com/br/engenharia/imc/Main.java
+++ IMC/src/com/br/engenharia/imc/Main.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.br.engenharia.imc;
 
 /**
  *
  * @author jessicagomesdossantos
  */
+//JFrame classe responsável por exibir a janela
+import javax.swing.JFrame;
+
+    //Main - Nome da classe
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Criação de um objeto ex da classe Ex2
+        Ex2 ex = new Ex2();
+        //posição da tela ao abri-la
+        ex.setLocation(400, 200);
+        //tornando a tela visível
+        ex.setVisible(true);
     }
-    
 }
